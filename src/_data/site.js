5ca20a9f538d4a47372202fe65fad454d73091db
James Steinbach <jdsteinbach@gmail.com>
2020-01-30 16:40:19 -0700

    dynamic copyright date

---
+++ src/_data/site.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: "The First-Century Roman Imperial Cult as Historical Context for Pauline Christology",
+  subtitle: "Proclaiming Christ as Lord in Caesar’s Empire",
+  year: new Date().getFullYear()
+}

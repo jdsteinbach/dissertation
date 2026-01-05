e7bafaad3254e3c8bf8d70418891109e864dcd5a
James Steinbach <jdsteinbach@gmail.com>
2026-01-05 10:49:21 -0700

    all the updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,71 +1,74 @@
-module.exports = eleventyConfig => {
+module.exports = (eleventyConfig) => {
   // Markdown
-  let markdownIt = require('markdown-it')
+  let markdownIt = require("markdown-it");
 
-  eleventyConfig.setLibrary('md', markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true,
-    typographer: true
-  }))
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({
+      html: true,
+      breaks: true,
+      linkify: true,
+      typographer: true,
+    })
+  );
 
-  eleventyConfig.addCollection('chapters', collection => {
+  eleventyConfig.addCollection("chapters", (collection) => {
     return collection
       .getAll()
-      .filter(page => {
-        return page.data.content_type === 'chapter'
+      .filter((page) => {
+        return page.data.content_type === "chapter";
       })
       .sort((a, b) => {
         return a.data.chapter_order === b.data.chapter_order
           ? 0
           : a.data.chapter_order < b.data.chapter_order
-            ? -1
-            : 1;
+          ? -1
+          : 1;
       })
-      .map(page => {
-        const children = collection.getAll().filter(item => {
-          return item.data.title === page.data.title
-        })
+      .map((page) => {
+        const children = collection.getAll().filter((item) => {
+          return item.data.title === page.data.title;
+        });
 
-        const outline = children.find(item => {
-          return item.data.content_type === 'outline'
-        })
+        const outline = children.find((item) => {
+          return item.data.content_type === "outline";
+        });
 
-        const introduction = children.find(item => {
-          return item.data.content_type === 'introduction'
-        })
+        const introduction = children.find((item) => {
+          return item.data.content_type === "introduction";
+        });
 
-        const conclusion = children.find(item => {
-          return item.data.content_type === 'conclusion'
-        })
+        const conclusion = children.find((item) => {
+          return item.data.content_type === "conclusion";
+        });
 
         page.data = {
           ...page.data,
           outline,
           introduction,
-          conclusion
-        }
+          conclusion,
+        };
 
-        return page
-      })
-  })
+        return page;
+      });
+  });
 
   // Netlify Redirect
-  eleventyConfig.addPassthroughCopy('_redirects')
+  eleventyConfig.addPassthroughCopy("_redirects");
 
   // Web Fonts
-  eleventyConfig.addPassthroughCopy('src/fonts')
+  eleventyConfig.addPassthroughCopy("src/fonts");
 
   //Favicon
-  eleventyConfig.addPassthroughCopy('src/favicon.ico')
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   return {
     dir: {
-      input: 'src',
-      data: '_data',
-      output: '_site'
+      input: "src",
+      data: "_data",
+      output: "_site",
     },
-    templateFormats: ['liquid', 'md', '11ty.js'],
-    passthroughFileCopy: true
-  }
-}
+    templateFormats: ["liquid", "md", "11ty.js"],
+    passthroughFileCopy: true,
+  };
+};
--- src/css/styles.11ty.js
+++ src/css/styles.11ty.js
@@ -1,36 +1,37 @@
-const fs = require('fs')
-const path = require('path')
-const postcss = require('postcss')
+const fs = require("fs");
+const path = require("path");
+const postcss = require("postcss");
 
 // the file name as an entry point for postcss compilation
 // also used to define the output filename in our output /css folder.
-const fileName = 'styles.css'
+const fileName = "styles.css";
 
 module.exports = class {
-  async data () {
-    const rawFilepath = path.join(__dirname, `../_includes/postcss/${fileName}`)
-    console.log(rawFilepath)
+  async data() {
+    const rawFilepath = path.join(
+      __dirname,
+      `../_includes/postcss/${fileName}`
+    );
+    console.log(rawFilepath);
     return {
       permalink: `css/${fileName}`,
       rawFilepath,
-      rawCss: await fs.readFileSync(rawFilepath)
-    }
+      rawCss: await fs.readFileSync(rawFilepath),
+    };
   }
 
-  async render ({ rawCss, rawFilepath }) {
+  async render({ rawCss, rawFilepath }) {
     return await postcss([
-      require('postcss-partial-import'),
-      require('postcss-nested'),
-      require('postcss-image-inliner')({
-        assetPaths: [
-          path.join(__dirname, `../_includes/img`)
-        ],
-        maxFileSize: 0
+      require("postcss-import"),
+      require("postcss-nested"),
+      require("postcss-image-inliner")({
+        assetPaths: [path.join(__dirname, `../_includes/img`)],
+        maxFileSize: 0,
       }),
-      require('autoprefixer'),
-      require('cssnano')
+      require("autoprefixer"),
+      require("cssnano"),
     ])
-    .process(rawCss, { from: rawFilepath })
-    .then(result => result.css)
+      .process(rawCss, { from: rawFilepath })
+      .then((result) => result.css);
   }
-}
+};
